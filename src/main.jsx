ebc7f13d3c3c97b2c51901c5708fbf63f3c253c1
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-05-12 16:28:49 +0530

    update design

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import "@vanilla-web-components/design";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './index.css';
 
 import App from './App.jsx';
